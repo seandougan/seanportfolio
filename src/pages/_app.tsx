4d3171c2604a4b694a2c7424ac83acf61973fefb
sean <svoogan@hotmail.com>
2023-02-08 14:56:10 -0500

    testsd

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,4 @@ import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
-  test
 }
