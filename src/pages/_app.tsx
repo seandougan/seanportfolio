7dec6fedfc02c5b810eac4863932c3c19049ba11
sean <svoogan@hotmail.com>
2023-02-08 15:30:31 -0500

    Sitemap, Robots,  Finalized Proper yarn scripts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,5 @@ import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
-  test
 }
-var empty = {};
+
