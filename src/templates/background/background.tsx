3e5f07993704992a187793adf585cdd698be2124
sean <svoogan@hotmail.com>
2023-02-08 18:16:10 -0500

    remove starter assets. Project structure

---
+++ src/templates/background/background.tsx
@@ -0,0 +1,33 @@
+
+
+export default function background(){
+  return(
+    <>
+      <div className="lamp">
+        <div className="lava">
+          <div className="blob"></div>
+          <div className="blob"></div>
+          <div className="blob"></div>
+          <div className="blob"></div>
+          <div className="blob"></div>
+          <div className="blob"></div>
+          <div className="blob"></div>
+          <div className="blob"></div>
+          <div className="blob top"></div>
+          <div className="blob bottom"></div>
+        </div>
+      </div>
+      <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+        <defs>
+          <filter id="goo">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="10" result="blur" />
+            <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7" result="goo" />
+            <feBlend in="SourceGraphic" in2="goo" />
+          </filter>
+        </defs>
+      </svg>
+      <h1 className="text-light">CSS Lava Lamp</h1>
+      <p className="text-light">Originally by Rose Lui</p>
+    </>
+  );
+}
